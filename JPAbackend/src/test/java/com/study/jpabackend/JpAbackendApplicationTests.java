985c443e31d35931006c55b1a5ee3ad201317f2f
KimJinA0120 <wlsdk0120@naver.com>
2026-07-28 10:37:40 +0900

    backend생성

---
+++ JPAbackend/src/test/java/com/study/jpabackend/JpAbackendApplicationTests.java
@@ -0,0 +1,13 @@
+package com.study.jpabackend;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class JpAbackendApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
